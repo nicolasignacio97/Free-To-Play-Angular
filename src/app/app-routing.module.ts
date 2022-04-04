c8fa906348475ea9afbb62fc0dc323ac20a9471a
Nicolas <nicoztk619@gmail.com>
2022-04-04 15:59:08 -0400

    1.0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes: Routes = [
-
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
   {
     path: 'personajes',
     loadChildren: () => import('./games/games.module').then(m => m.GamesModule)
   },
-
+  {
+    path: '404',
+    component: ErrorPageComponent
+  },
   {
     path: '**',
-    redirectTo: 'personajes/listado'
+    redirectTo: 'personajes'
   },
 ];
 
